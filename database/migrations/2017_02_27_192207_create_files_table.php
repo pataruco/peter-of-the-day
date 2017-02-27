64d6fd29599ed09af999720197343518999b944d
Pedro Martin <pataruco@gmail.com>
2017-02-27 21:42:16 +0000

    Create tables and migrations

--- database/migrations/2017_02_27_192207_create_files_table.php
+++ database/migrations/2017_02_27_192207_create_files_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateUsersTable extends Migration
+class CreateFilesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,16 +13,17 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('files', function (Blueprint $table) {
             $table->increments('id');
-            $table->string('name');
-            $table->string('email');
-            $table->string('password');
-            $table->rememberToken();
             $table->timestamps();
+            $table->string('url')->nullable();
+            $table->text('description')->nullable();
+            $table->string('name')->nullable();
+            $table->string('filename')->nullable();
+            $table->enum('choices', ['picture', 'video'] );
+            $table->integer('day_id');
         });
     }
-
     /**
      * Reverse the migrations.
      *
@@ -30,6 +31,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('files');
     }
 }
