c3d94c332d8cbcbafe6502556120847e950a7474
Pedro Martin <pataruco@gmail.com>
2017-02-28 22:45:31 +0000

    Render days and files

--- resources/views/days/index.blade.php
+++ resources/views/days/index.blade.php
@@ -1,1 +1,15 @@
-<h1>Hola</h1>
+<ul>
+    @foreach ($days as $day  )
+        <li>
+            <time>{{ $day->date }}</time>
+            @foreach ( $day->files as $file )
+                <ul>
+                    <li>
+                        <a href="{{ $file->url }}">{{ $file->name }}</a>
+                    </li>
+
+                </ul>
+            @endforeach
+        </li>
+    @endforeach
+</ul>
