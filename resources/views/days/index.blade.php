8cb765d77c9350884781407b38fc6df49642d6a0
Pedro Martin <pataruco@gmail.com>
2017-03-01 21:36:34 +0000

    Draft link to Day show view

--- resources/views/days/index.blade.php
+++ resources/views/days/index.blade.php
@@ -1,13 +1,14 @@
 <ul>
     @foreach ($days as $day  )
         <li>
-            <time>{{ $day->date }}</time>
+            <a href="/days/{{ $day->id }}">
+                <time>{{ $day->date }}</time>
+            </a>
             @foreach ( $day->files as $file )
                 <ul>
                     <li>
                         <a href="{{ $file->url }}">{{ $file->name }}</a>
                     </li>
-                    
                 </ul>
             @endforeach
         </li>
