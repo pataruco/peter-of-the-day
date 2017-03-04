147afc510b5b93fad52cb021c2ca3451f861b4ef
Pedro Martin <pataruco@gmail.com>
2017-03-04 17:21:04 +0000

    Abstract fillables from form
    Extract fillable from create form to reuse it on edit view

--- resources/views/days/create.blade.php
+++ resources/views/days/create.blade.php
@@ -1,10 +1,6 @@
 {{ Form::open( [ 'route' => 'days.store', 'files' => true ] ) }}
 
-    {{ Form::label('date', 'Date') }}
-    {{ Form::date('date', \Carbon\Carbon::now()) }}
-
-    {{ Form::label('files[]', 'Choose files') }}
-    {{ Form::file('files[]',  [ 'multiple' => true ] ) }}
+    @include('days.form')
 
     {{ Form::submit('Add Days') }}
 
