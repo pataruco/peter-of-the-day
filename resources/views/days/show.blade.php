e16144dcad3a47ce16942fdda4b001992f134b82
Pedro Martin <pataruco@gmail.com>
2017-03-12 17:18:50 +0000

    Create a delete form for Day

--- resources/views/days/show.blade.php
+++ resources/views/days/show.blade.php
@@ -1,27 +1,30 @@
 <time>{{ $day->date }}</time>
 
 <a href="{{ route('days.edit', [ 'id' => $day->id ] ) }}">Edit</a>
-
-<h2>Video</h2>
-    @if ( isset( $videos) )
-    <ul>
-        @foreach ($videos as $video)
-            <video src="{{ $video->url }}" autoplay controls>
-
-            </video>
-        @endforeach
-    </ul>
-    @endif
+{!!  Form::model($day, ['route' => ['days.destroy', $day->id], 'method' => 'delete'] ) !!}
+        {{ Form::submit('Delete Day') }}
+{{ Form::close() }}
 
 
-<h2>Images</h2>
 
 @if ( isset( $images ) )
+    <h2>Images</h2>
     <ul>
         @foreach ($images as $image)
             <li>
                 <img src="{{ $image->url }}" alt="">
             </li>
+        @endforeach
+    </ul>
+@endif
+
+@if ( isset( $videos) )
+    <h2>Video</h2>
+    <ul>
+        @foreach ($videos as $video)
+            <video src="{{ $video->url }}" controls>
+
+            </video>
         @endforeach
     </ul>
 @endif
