d4f5bd7792263dea39ea68a6bba2ec4d8603667e
Pedro Martin <pataruco@gmail.com>
2017-03-12 17:18:38 +0000

    Create a a edit form

--- resources/views/days/edit.blade.php
+++ resources/views/days/edit.blade.php
@@ -1,5 +1,4 @@
-{{ Form::model( $day, [ 'route' => [ 'days.update', $day ], 'files' => true ] ) }}
-
+{!!  Form::model($day, ['route' => ['days.update', $day->id], 'method' => 'put', 'files' => true] ) !!}
     @include('days.form')
 
     {{ Form::submit('Update Day') }}
@@ -7,3 +6,25 @@
 {{ Form::close() }}
 
 @include('errors.error-bag')
+
+@if ( isset( $images ) )
+    <h2>Images</h2>
+    <ul>
+        @foreach ($images as $image)
+            <li>
+                <img src="{{ $image->url }}" alt="">
+            </li>
+        @endforeach
+    </ul>
+@endif
+
+@if ( isset( $videos) )
+    <h2>Video</h2>
+    <ul>
+        @foreach ($videos as $video)
+            <video src="{{ $video->url }}" controls>
+
+            </video>
+        @endforeach
+    </ul>
+@endif
