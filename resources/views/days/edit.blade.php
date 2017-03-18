022a3c3ac17c2927f023dd9ee96611c791f35d45
Pedro Martin <pataruco@gmail.com>
2017-03-18 19:10:37 +0000

    Extend laravel layout

--- resources/views/days/edit.blade.php
+++ resources/views/days/edit.blade.php
@@ -1,36 +1,41 @@
-{!!  Form::open(['route' => ['days.update', $day->id], 'method' => 'put', 'files' => true] ) !!}
-    @include('days.form')
+@extends('layouts.app')
 
-    {{ Form::submit('Update Day') }}
+@section('content')
 
-{{ Form::close() }}
+    {!!  Form::open(['route' => ['days.update', $day->id], 'method' => 'put', 'files' => true] ) !!}
+        @include('days.form')
 
-@include('errors.error-bag')
+        {{ Form::submit('Update Day') }}
 
-@if ( !empty( $images ) )
-    <h2>Images</h2>
-    <ul>
-        @foreach ($images as $image)
-            <li>
-                <img src="{{ $image->url }}" alt="">
-                {!!  Form::open(['route' => ['files.destroy', $image->id], 'method' => 'delete'] ) !!}
-                        {{ Form::submit('Delete Image') }}
+    {{ Form::close() }}
+
+    @include('errors.error-bag')
+
+    @if ( !empty( $images ) )
+        <h2>Images</h2>
+        <ul>
+            @foreach ($images as $image)
+                <li>
+                    <img src="{{ $image->url }}" alt="">
+                    {!!  Form::open(['route' => ['files.destroy', $image->id], 'method' => 'delete'] ) !!}
+                            {{ Form::submit('Delete Image') }}
+                    {{ Form::close() }}
+                </li>
+            @endforeach
+        </ul>
+    @endif
+
+    @if ( !empty( $videos) )
+        <h2>Video</h2>
+        <ul>
+            @foreach ($videos as $video)
+                <video src="{{ $video->url }}" controls>
+
+                </video>
+                {!!  Form::open(['route' => ['files.destroy', $video->id], 'method' => 'delete'] ) !!}
+                        {{ Form::submit('Delete video') }}
                 {{ Form::close() }}
-            </li>
-        @endforeach
-    </ul>
-@endif
-
-@if ( !empty( $videos) )
-    <h2>Video</h2>
-    <ul>
-        @foreach ($videos as $video)
-            <video src="{{ $video->url }}" controls>
-
-            </video>
-            {!!  Form::open(['route' => ['files.destroy', $video->id], 'method' => 'delete'] ) !!}
-                    {{ Form::submit('Delete video') }}
-            {{ Form::close() }}
-        @endforeach
-    </ul>
-@endif
+            @endforeach
+        </ul>
+    @endif
+@endsection
