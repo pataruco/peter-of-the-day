89f602648bb6b8709d30c173eb29591802e2fef7
Pedro Martin <pataruco@gmail.com>
2017-03-29 20:19:13 +0100

    Reset layout and style to use bootstrap :angry:

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,62 +1,68 @@
 @extends('layouts.app')
 
 @section('content')
-    <main class="row">
-        <div class="constrain column">
-            <h2>Login</h2>
-            <form class="form" role="form" method="POST" action="{{ route('login') }}">
-                {{ csrf_field() }}
-                <div class="form__group">
-                    <label for="email" class="">E-Mail Address</label>
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Login</div>
+                <div class="panel-body">
+                    <form class="form-horizontal" role="form" method="POST" action="{{ route('login') }}">
+                        {{ csrf_field() }}
 
-                    <div class="form__input">
-                        <input id="email" type="email" class="" name="email" value="{{ old('email') }}" required autofocus>
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
 
-                        @if ($errors->has('email'))
-                            <span class="">
-                                <strong>{{ $errors->first('email') }}</strong>
-                            </span>
-                        @endif
-                    </div>
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">Password</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <div class="checkbox">
+                                    <label>
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <div class="col-md-8 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Login
+                                </button>
+
+                                <a class="btn btn-link" href="{{ route('password.request') }}">
+                                    Forgot Your Password?
+                                </a>
+                            </div>
+                        </div>
+                    </form>
                 </div>
-
-                <div class="form__group">
-                    <label for="password" class="">Password</label>
-
-                    <div class="form__input">
-                        <input id="password" type="password" class="" name="password" required>
-
-                        @if ($errors->has('password'))
-                            <span class="">
-                                <strong>{{ $errors->first('password') }}</strong>
-                            </span>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="form__group">
-                    <div class="form__input">
-                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }} class="form__checkbox">
-                        <label>
-                            Remember Me
-                        </label>
-                    </div>
-                </div>
-
-                <div class="form__group">
-                    <div class="form__input">
-                        <button type="submit" class="btn btn--primary">
-                            Login
-                        </button>
-
-                        <a class="" href="{{ route('password.request') }}">
-                            Forgot Your Password?
-                        </a>
-                    </div>
-                </div>
-            </form>
-
-        </main>
-
+            </div>
         </div>
+    </div>
+</div>
 @endsection
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -21,45 +21,65 @@
     </script>
 </head>
 <body>
-    <header class="navigation__header">
-        <nav class="navigation navigation--align-right row">
-            <div class="column column--half">
-                <h1 class="navigation__brand">
-                    <a class="" href="{{ url('/') }}">
+    <div id="app">
+        <nav class="navbar navbar-default navbar-static-top">
+            <div class="container">
+                <div class="navbar-header">
+
+                    <!-- Collapsed Hamburger -->
+                    <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#app-navbar-collapse">
+                        <span class="sr-only">Toggle Navigation</span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                        <span class="icon-bar"></span>
+                    </button>
+
+                    <!-- Branding Image -->
+                    <a class="navbar-brand" href="{{ url('/') }}">
                         {{ config('app.name', 'Laravel') }}
                     </a>
-                </h1>
-            </div>
-            <div class="column column--half">
-                <ul class="navigation__login row">
-                    @if (Auth::guest())
-                        <li class="column column--half"><a href="{{ route('login') }}">Login</a></li>
-                        <li class="column column--half"><a href="{{ route('register') }}">Register</a></li>
-                    @else
-                        <li class="" id="js-toggle-slide-button">
-                            <a href="#" class="navigation__username btn btn--black btn--no-border" role="button" aria-expanded="false">
-                                {{ Auth::user()->name }}
-                            </a>
-                            <ul class="navigation__logged" role="" id="js-toggle-slide-container">
-                                <li>
-                                    <a href="{{ route('logout') }}" class="btn btn--no-border btn--black" onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                                    Logout
-                                    </a>
-                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                        {{ csrf_field() }}
-                                    </form>
-                                </li>
-                            </ul>
-                        </li>
-                    @endif
-                </ul>
+                </div>
+
+                <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="nav navbar-nav">
+                        &nbsp;
+                    </ul>
+
+                    <!-- Right Side Of Navbar -->
+                    <ul class="nav navbar-nav navbar-right">
+                        <!-- Authentication Links -->
+                        @if (Auth::guest())
+                            <li><a href="{{ route('login') }}">Login</a></li>
+                            <li><a href="{{ route('register') }}">Register</a></li>
+                        @else
+                            <li class="dropdown">
+                                <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                    {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <ul class="dropdown-menu" role="menu">
+                                    <li>
+                                        <a href="{{ route('logout') }}"
+                                            onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();">
+                                            Logout
+                                        </a>
+
+                                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                                            {{ csrf_field() }}
+                                        </form>
+                                    </li>
+                                </ul>
+                            </li>
+                        @endif
+                    </ul>
+                </div>
             </div>
         </nav>
-    </header>
 
         @yield('content')
-
+    </div>
 
     <!-- Scripts -->
     <script src="{{ asset('js/app.js') }}"></script>
