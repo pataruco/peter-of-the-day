1a0f426b93e2514cd840df2b1fe5f6290f6aea16
Pedro Martin <pataruco@gmail.com>
2017-04-01 18:17:26 +0100

    WIP: backup

---
+++ app/Http/Controllers/DayApiController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Day;
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+use Storage;
+
+class DayApiController extends Controller
+{
+    public function today( )
+    {
+        $today = Carbon::create()->toDateString();
+        $day = Day::where('date', $today)->get()->first();
+        if ( $day ) {
+            $image = $day->images()[0];
+            $file = Storage::disk('s3')->get( $image->setPath() );
+            $headers = [
+                 'Content-Type' => 'image',
+                 'Content-Disposition' => 'attachment',
+                 'filename'=>$image->filename
+             ];
+            return response()->file($file, $image->filename, $headers );
+        } else {
+            dump('no day');
+        }
+    }
+}
+
+
+// public function respondDownload($fileContent, $fileName, $mime)
+// {
+//     $headers = [
+//         'Content-Type' => $mime,
+//         'Content-Disposition' => 'attachment',
+//         'filename'=>$fileName
+//     ];
+//     return response()->download($fileContent, $fileName, $headers);
+// }
--- routes/api.php
+++ routes/api.php
@@ -16,3 +16,14 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::get('/day/today', 'DayApiController@today');
+
+
+
+// /api/day/today ->random today picture || no picture message
+// /api/day/{date}/ -> number of pictures || no picture message
+// /api/day/{date}/random -> picture url || no picture message
+// /api/day/{date}/{number} ->picture url || no picture message
+// /api/day/{date}/random/download -> picture download
+// /api/day/{date}/{number}/download ->picture
