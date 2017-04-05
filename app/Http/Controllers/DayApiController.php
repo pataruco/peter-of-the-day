7374ddb06d65d83629110a9b3d6d1ac27b6012f9
Pedro Martin <pataruco@gmail.com>
2017-04-05 21:48:16 +0100

    Create raw API routes

--- app/Http/Controllers/DayApiController.php
+++ app/Http/Controllers/DayApiController.php
@@ -22,4 +22,55 @@ class DayApiController extends Controller
             dump('no day');
         }
     }
+
+    public function date( $date ) {
+        $day = Day::where('date', $date)->get()->first();
+        if ( $day ) {
+            $count =  count( $day->images() );
+            return response()->json([
+                'date' =>  $day->date,
+                'count' => $count
+            ]);
+        } else {
+            dump('no day');
+        }
+    }
+
+    public function random( $date ) {
+        $day = Day::where('date', $date)->get()->first();
+        if ( $day ) {
+            $max =  count( $day->images() );
+            $random = random_int( $min , $max );
+            $image = $day->images()[$random];
+            $file = Storage::disk('s3')->get( $image->setPath() );
+            return Image::make( $file  )->response();
+        } else {
+            dump('no day');
+        }
+    }
+
+    public function dateNumber( $date, $number ){
+        $day = Day::where('date', $date)->get()->first();
+        if ( $day ) {
+            $max =  count( $day->images() );
+            $image = $day->images()[$number];
+            $file = Storage::disk('s3')->get( $image->setPath() );
+            return Image::make( $file  )->response();
+        } else {
+            dump('no day');
+        }
+    }
+
+    public function randomDownload( ) {
+        $day = Day::where('date', $date)->get()->first();
+        if ( $day ) {
+            $max =  count( $day->images() );
+            $random = random_int( $min , $max );
+            $image = $day->images()[$random];
+            // $file = Storage::disk('s3')->get( $image->setPath() );
+            return response()->download( $image->setPath() );
+        } else {
+            dump('no day');
+        }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,13 +17,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// /api/day/today ->random today picture || no picture message
 Route::get('/day/today', 'DayApiController@today');
 
-
-
-// /api/day/today ->random today picture || no picture message
 // /api/day/{date}/ -> number of pictures || no picture message
+Route::get('/day/{date}', 'DayApiController@date')->where('date', '[A-Z]+|\d{4}(?:-\d{1,2}){2}');
+
 // /api/day/{date}/random -> picture url || no picture message
+Route::get('/day/{date}/random', 'DayApiController@random')->where('date', '[A-Z]+|\d{4}(?:-\d{1,2}){2}');
 // /api/day/{date}/{number} ->picture url || no picture message
+Route::get('/day/{date}/{number}', 'DayApiController@dateNumber')->where('date', '[A-Z]+|\d{4}(?:-\d{1,2}){2}');
 // /api/day/{date}/random/download -> picture download
+Route::get('/day/{date}/random/download', 'DayApiController@randomDownload')->where('date', '[A-Z]+|\d{4}(?:-\d{1,2}){2}');  
 // /api/day/{date}/{number}/download ->picture
