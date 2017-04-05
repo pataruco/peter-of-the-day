89c1ed93e91a9894d5037fb1b00f18c1ca0f943d
Pedro Martin <pataruco@gmail.com>
2017-04-05 19:14:20 +0100

    Remove full calendar

--- resources/assets/js/bootstrap.js
+++ resources/assets/js/bootstrap.js
@@ -10,7 +10,6 @@ window._ = require('lodash');
 window.$ = window.jQuery = require('jquery');
 
 require('bootstrap-sass');
-require('fullcalendar');
 
 /**
  * Vue is a modern JavaScript library for building interactive web interfaces
--- resources/assets/js/partials/calendar.js
+++ resources/assets/js/partials/calendar.js
@@ -1,100 +1,55 @@
-// const calendar = new Object();
-//
-// moment.locale('es');
-// calendar.$container = $('#js-calendar-container');
-//
-// calendar.peterBday = moment("26-01-2017", "DD-MM-YYYY");
-// calendar.today = moment();
-//
-// calendar.datesSelect = function (target) {
-//     console.log();
-//     if ( target.events.length ) {
-//         let dateId = target.events[0].id
-//         window.location = `/days/${dateId}`;
-//     }
-// }
-//
-// calendar.settings = {
-//     startWithMonth: calendar.today,
-//     moment: moment,
-//     trackSelectedDate: true,
-//     constraints: {
-//         startDate: calendar.peterBday,
-//         endDate: calendar.today
-//     },
-//     clickEvents: {
-//         click: function (target) {
-//             calendar.datesSelect ( target )
-//         }
-//     },
-//     classes: {
-//         past: "past",
-//         today: "today",
-//         event: "event",
-//         selected: "selected",
-//         inactive: "inactive",
-//         lastMonth: "last-month",
-//         nextMonth: "next-month",
-//         adjacentMonth: "adjacent-month",
-//     },
-//     weekOffset: 0
-// }
-//
-// calendar.render = function ( dates ) {
-//     calendar.settings.events = dates;
-//     calendar.$container.clndr(calendar.settings);
-// }
-//
-// calendar.getDays = function () {
-//     axios.get('/json/days')
-//         .then( ( response ) => {
-//             let dates = response.data;
-//             calendar.render( dates )
-//         })
-//         .catch( ( error )=>  {
-//             console.error( error );
-//         });
-// }
-//
-// if ( calendar.$container.length ) {
-//     $(window).on('load', calendar.getDays);
-// }
-//
-// $( document ).ready( calendar );
-
-
-// Using full calendar
-
 const calendar = new Object();
 
+moment.locale('es');
 calendar.$container = $('#js-calendar-container');
 
-calendar.render = function ( dates ) {
-    calendar.$container.fullCalendar({
-        events: dates
-    });
+calendar.peterBday = moment("26-01-2017", "DD-MM-YYYY");
+calendar.today = moment();
+
+calendar.datesSelect = function (target) {
+    console.log();
+    if ( target.events.length ) {
+        let dateId = target.events[0].id
+        window.location = `/days/${dateId}`;
+    }
 }
 
-calendar.changeKeyOnDays = function ( dates ) {
-    newDates = new Array();
-    dates.forEach( function changeKeyValue( date ) {
-        let newDate = {
-            url: `/days/${date.id}`,
-            date: date.date
+calendar.settings = {
+    startWithMonth: calendar.today,
+    moment: moment,
+    trackSelectedDate: true,
+    constraints: {
+        startDate: calendar.peterBday,
+        endDate: calendar.today
+    },
+    clickEvents: {
+        click: function (target) {
+            calendar.datesSelect ( target )
         }
-        newDates.push( newDate );
-    })
-    calendar.render( newDates )
+    },
+    classes: {
+        past: "past",
+        today: "today",
+        event: "event",
+        selected: "selected",
+        inactive: "inactive",
+        lastMonth: "last-month",
+        nextMonth: "next-month",
+        adjacentMonth: "adjacent-month",
+    },
+    weekOffset: 0
 }
 
+calendar.render = function ( dates ) {
+    calendar.settings.events = dates;
+    calendar.$container.clndr(calendar.settings);
+}
 
 calendar.getDays = function () {
     axios.get('/json/days')
         .then( ( response ) => {
             let dates = response.data;
-            // console.log(dates);
-            calendar.changeKeyOnDays( dates );
-            // calendar.render( dates );
+            calendar.render( dates )
         })
         .catch( ( error )=>  {
             console.error( error );
