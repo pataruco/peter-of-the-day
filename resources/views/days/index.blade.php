67e0b2e06cf2e9aaa746b106e33bcf78f119de66
Pedro Martin <pataruco@gmail.com>
2017-04-09 15:02:53 +0100

    Move add day to all views

--- resources/views/days/index.blade.php
+++ resources/views/days/index.blade.php
@@ -2,13 +2,6 @@
 
 @section('content')
     <main class="container">
-        <section class="row">
-            <div class="col-md-12 text-right">
-                @can('create', App\Day::class)
-                    <a class="btn btn-default" href="{{ route('days.create') }}">Add a new day</a>
-                @endcan
-            </div>
-        </section>
 
         <section id="js-calendar-container">
 
