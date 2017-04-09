e318a5989a8fd7610877a4b0e3aed294016dea0b
Pedro Martin <pataruco@gmail.com>
2017-04-09 12:58:36 +0100

    Move logic to Model

--- app/Http/Controllers/DayApiController.php
+++ app/Http/Controllers/DayApiController.php
@@ -14,7 +14,7 @@ class DayApiController extends Controller
     public function today( )
     {
         $today = Carbon::create()->toDateString();
-        $image = $this->getImage($today);
+        $image = Day::getImage($today);
         if ( $image ) {
             return $image->response();
         } else {
@@ -25,7 +25,7 @@ class DayApiController extends Controller
     }
 
     public function date( $date ) {
-        $day = $this->getDay( $date );
+        $day = Day::getDay( $date );
         if ( $day ) {
             $count =  count( $day->images() );
             return response()->json([
@@ -40,7 +40,7 @@ class DayApiController extends Controller
     }
 
     public function random( $date ) {
-        $image = $this->getImage( $date, 0,  true);
+        $image = Day::getImage( $date, 0,  true);
         if ( $image ) {
             return $image->response();
         } else {
@@ -51,7 +51,7 @@ class DayApiController extends Controller
     }
 
     public function dateNumber( $date, $number ){
-        $image = $this->getImage( $date, $number,  false );
+        $image = Day::getImage( $date, $number,  false );
         if ( $image ) {
             return $image->response();
         } else {
@@ -62,7 +62,7 @@ class DayApiController extends Controller
     }
 
     public function randomDownload( $date ) {
-        $image = $this->getImage( $date, 0,  true, true );
+        $image = Day::getImage( $date, 0,  true, true );
         if ( $image ) {
             return response( $image['file'], $image['response'], $image['headers'] );
         } else {
@@ -73,58 +73,13 @@ class DayApiController extends Controller
     }
 
     public function dateNumberDownload( $date, $number ) {
-        $image = $this->getImage( $date, $number,  false, true );
+        $image = Day::getImage( $date, $number,  false, true );
         if ( $image ) {
             return response( $image['file'], $image['response'], $image['headers'] );
         } else {
             return response()->json([
                   'message' =>  'No image today',
               ]);
-        }
-    }
-
-    public function getDay( $date ) {
-        $day = Day::where('date', $date)->get()->first();
-        if ( $day ) {
-            return $day;
-        } else {
-            return false;
-        }
-    }
-
-
-    public function getImage( $date, $number = false, $random = false, $download = false ) {
-        $day = $this->getDay( $date );
-        if ( $day ) {
-            $max =  count( $day->images() );
-            if ( $random ) {
-                $number = random_int( 0 , $max - 1 );
-            }
-            if ( $number ) {
-                if ( $number >= $max || $number <= -1 ) {
-                    return false;
-                } else {
-                    $number = $number -1;
-                }
-            }
-            $image = $day->images()[$number];
-            $file = Storage::disk('s3')->get( $image->setPath() );
-            if ( $download ) {
-                $headers = [
-                    'Content-Type' => 'image/png',
-                    'Content-Description' => 'File Transfer',
-                    'Content-Disposition' => "attachment; filename={$image->filename}",
-                    'filename'=> $image->filename
-                ];
-                return array(
-                    'file' => $file,
-                    'response'=>200,
-                    'headers' => $headers
-                );
-            }
-            return Image::make( $file );
-        } else {
-            return false;
         }
     }
 }
