5319c1b66fa50e2bb75f787728effd8a1642bff2
Pedro Martin <pataruco@gmail.com>
2017-04-23 20:30:15 +0100

    Style buttons

--- resources/views/days/partials/root.blade.php
+++ resources/views/days/partials/root.blade.php
@@ -1,7 +1,7 @@
 <section class="container">
     <div class="row">
         <div class="col-md-12 text-right">
-            <a class="btn btn-return" href="{{ route('days.index') }}">
+            <a class="btn btn-circle" href="{{ route('days.index') }}">
                 <span class="glyphicon glyphicon-remove"></span>
             </a>
         </div>
